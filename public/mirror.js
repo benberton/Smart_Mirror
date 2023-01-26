efb1cd4f997d7efe0ede47438ebdce0702820b94
benberton <bberton@zagmail.gonzaga.edu>
2023-01-25 19:15:41 -0800

    basic clock added

--- public/mirror.js
+++ public/mirror.js
@@ -1,11 +1,23 @@
 document.addEventListener("DOMContentLoaded", ()=>{
-    let date = Date()
-    document.getElementById("time").innerHTML = date.toString()
+
+    setInterval(setTime, 300);
 });
 
+function setTime()
+{
+    const date = new Date()
+    
+    document.getElementById("time").innerHTML = date.getHours() + ":" + convertToTime(date.getMinutes())
+    document.getElementById("seconds").innerHTML = convertToTime(date.getSeconds())
+}
 
-
-
+// if the time is less that 10, a zero is added in front (ex: 8 -> 08)
+function convertToTime(time)
+{
+    if (time < 10)
+        return "0" + time
+    return time
+}
 
 
 
