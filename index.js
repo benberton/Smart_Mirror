b322c47750ef2a0e44e81890eff0f13370b894d2
benberton <bberton@zagmail.gonzaga.edu>
2023-02-01 20:36:32 -0800

    hiding token

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 8888
 // const port = 8080
 const app = express()
 
-var SpotifyWebApi = require('spotify-web-api-node');
+var SpotifyclearWebApi = require('spotify-web-api-node');
 
 const scopes = [
     'ugc-image-upload',
@@ -35,63 +35,63 @@ var spotifyApi = new SpotifyWebApi({
     clientId: '28cafb8e6ad643abb3c590097c498b8d',
     clientSecret: 'fc7c371dc50241b49afea980c72d5aaa',
     redirectUri: 'http://localhost:8888/callback'
-  });
+});
   
   
-  app.get('/login', (req, res) => {
+app.get('/login', (req, res) => {
     res.redirect(spotifyApi.createAuthorizeURL(scopes));
-  });
+});
   
-  app.get('/callback', (req, res) => {
+app.get('/callback', (req, res) => {
     const error = req.query.error;
     const code = req.query.code;
     const state = req.query.state;
-  
+
     if (error) {
-      console.error('Callback Error:', error);
-      res.send(`Callback Error: ${error}`);
-      return;
+        console.error('Callback Error:', error);
+        res.send(`Callback Error: ${error}`);
+        return;
     }
-  
+
     spotifyApi
-      .authorizationCodeGrant(code)
-      .then(data => {
+    .authorizationCodeGrant(code)
+    .then(data => {
+    const access_token = data.body['access_token'];
+    const refresh_token = data.body['refresh_token'];
+    const expires_in = data.body['expires_in'];
+
+    spotifyApi.setAccessToken(access_token);
+    spotifyApi.setRefreshToken(refresh_token);
+
+    console.log('access_token:', access_token);
+    console.log('refresh_token:', refresh_token);
+
+    console.log(
+        `Sucessfully retreived access token. Expires in ${expires_in} s.`
+    );
+    res.send('Success! You can now close the window.');
+
+    setInterval(async () => {
+        const data = await spotifyApi.refreshAccessToken();
         const access_token = data.body['access_token'];
-        const refresh_token = data.body['refresh_token'];
-        const expires_in = data.body['expires_in'];
-  
-        spotifyApi.setAccessToken(access_token);
-        spotifyApi.setRefreshToken(refresh_token);
-  
+
+        console.log('The access token has been refreshed!');
         console.log('access_token:', access_token);
-        console.log('refresh_token:', refresh_token);
-  
-        console.log(
-          `Sucessfully retreived access token. Expires in ${expires_in} s.`
-        );
-        res.send('Success! You can now close the window.');
-  
-        setInterval(async () => {
-          const data = await spotifyApi.refreshAccessToken();
-          const access_token = data.body['access_token'];
-  
-          console.log('The access token has been refreshed!');
-          console.log('access_token:', access_token);
-          spotifyApi.setAccessToken(access_token);
-        }, expires_in / 2 * 1000);
-      })
-      .catch(error => {
-        console.error('Error getting Tokens:', error);
-        res.send(`Error getting Tokens: ${error}`);
-      });
-  });
+        spotifyApi.setAccessToken(access_token);
+    }, expires_in / 2 * 1000);
+    })
+    .catch(error => {
+    console.error('Error getting Tokens:', error);
+    res.send(`Error getting Tokens: ${error}`);
+    });
+});
 
 
-
-app.use(express.static('public'))
-//using json format
-app.use(express.json());
-
+app.post("/api/getCurSong", function(req,res) {
+    console.log(req.data)
+    res.send(JSON.stringify({"song": Date().toISOString()}))
+    res.end()
+})
 
 app.listen(port,function(error) {
     if (error)
